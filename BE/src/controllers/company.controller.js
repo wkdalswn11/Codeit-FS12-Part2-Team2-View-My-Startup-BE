fd9e37e2ed83f95f8a9fd49f4041d8d8ef5096c7
apple <apple@apples-MacBook-Air.local>
2026-04-13 17:50:19 +0900

    fix: routes에 controller 함수 연결

--- BE/src/controllers/company.controller.js
+++ BE/src/controllers/company.controller.js
@@ -3,7 +3,7 @@ import { getCompaniesService } from "../services/company.service.js";
 export const getCompaniesController = async (req, res) => {
   try {
     const companies = await getCompaniesService(req.query);
-    res.status(200).json({ data: companies });
+    res.status(200).json({ data: companies.data, meta: companies.meta });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- BE/src/routes/company.routes.js
+++ BE/src/routes/company.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
+import { getCompaniesController } from "../controllers/company.controller.js";
 
 const router = express.Router();
-router.get("/");
+router.get("/", getCompaniesController);
 
 export default router;
