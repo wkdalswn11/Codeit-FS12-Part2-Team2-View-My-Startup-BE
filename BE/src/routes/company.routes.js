8289b9a4a4220d61500bc9078b523b4d1c912ebb
apple <apple@apples-MacBook-Air.local>
2026-04-16 14:32:49 +0900

    fix: 사이트 내에서 해당 기업의 총 투자금액 추가

--- BE/src/routes/company.routes.js
+++ BE/src/routes/company.routes.js
@@ -2,10 +2,12 @@ import express from "express";
 import {
   getCompaniesController,
   getCompanyByIdController,
+  getCompanyInvestmentsController,
 } from "../controllers/company.controller.js";
 
 const router = express.Router();
 router.get("/", getCompaniesController);
 router.get("/:companyId", getCompanyByIdController);
+router.get("/:companyId/investments", getCompanyInvestmentsController);
 
 export default router;
