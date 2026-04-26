e81cbdad4ebc90e333f9daf70fe739f383229df3
wkdalswn11 <wkdalswn194@gamil.com>
2026-04-26 17:53:24 +0900

    fix: 트렌드 컨트롤러 에러수정

--- BE/src/routes/company.routes.js
+++ BE/src/routes/company.routes.js
@@ -4,10 +4,11 @@ import {
   getCompaniesController,
   getCompanyByIdController,
   getCompanyInvestmentsController,
+  getTrendingController,
 } from "../controllers/company.controller.js";
 
 const router = express.Router();
-router.get("/trending")
+router.get("/trending", getTrendingController);
 router.get("/", getCompaniesController);
 router.get("/:companyId/investments", getCompanyInvestmentsController);
 router.post("/:companyId/investments", addCompanyInvestmentController);
